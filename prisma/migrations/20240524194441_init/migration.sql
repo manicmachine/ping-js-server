3ac810618babfc9cf33515272e080b4add5f4c79
Corey Oliphant <coreysather@gmail.com>
2024-09-13 09:19:55 -0500

    Create migration.sql

---
+++ prisma/migrations/20240524194441_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateEnum
+CREATE TYPE "Proto" AS ENUM ('ICMP', 'TCP');
+
+-- CreateEnum
+CREATE TYPE "MonitorTrigger" AS ENUM ('ONLINE', 'OFFLINE');
+
+-- CreateTable
+CREATE TABLE "MonitorDevice" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "port" INTEGER,
+    "proto" "Proto" NOT NULL DEFAULT 'ICMP',
+    "persist" BOOLEAN NOT NULL,
+    "monitor_trigger" "MonitorTrigger" NOT NULL,
+    "monitor_start_utc" INTEGER NOT NULL,
+    "monitor_end_utc" INTEGER NOT NULL,
+    "requested_by" TEXT NOT NULL,
+    "notify" TEXT NOT NULL,
+    "comments" TEXT,
+    "email_subject" TEXT NOT NULL,
+    "email_body" TEXT NOT NULL,
+    "been_notified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "MonitorDevice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "username" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("username")
+);
